b29b910843cf4e9d57cd355e0ebf0dc544e4c259
Gabriel Fanelli <gabrielfanelli61@gmail.com>
2019-09-07 00:22:35 -0300

    First commit, disassembler implemented up to opcode 0x2a

---
+++ 8080disassembler.c
@@ -0,0 +1,313 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  *codebuffer is a valid pointer to 8080 assembly code
+  pc is the current offset into the code
+
+  returns the number of bytes of the op
+*/
+
+int Disassemble8080Op(unsigned char *codebuffer, int pc)
+{
+    unsigned char *code = &codebuffer[pc];
+    int opbytes = 1;
+    printf("%04x ", pc);
+    switch(*code)
+    {
+        case 0x00: printf("NOP"); break;
+        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes = 3; break;
+        case 0x02: printf("STAX   B"); break;
+        case 0x03: printf("INX    B"); break;
+        case 0x04: printf("INR    B"); break;
+        case 0x05: printf("DCR    B"); break;
+        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes = 2; break;
+        case 0x07: printf("RLC"); break;
+        case 0x08: printf("NOP"); break;
+        case 0x09: printf("DAD    B"); break;
+        case 0x0a: printf("LDAX   B"); break;
+        case 0x0b: printf("DCX    B"); break;
+        case 0x0c: printf("INR    C"); break;
+        case 0x0d: printf("DCR    C"); break;
+        case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes = 2; break;
+        case 0x0f: printf("RRC"); break;
+        case 0x10: printf("NOP"); break;
+        case 0x11: printf("LXI    D,#$%02x%02x", code[2], code[1]); opbytes = 3; break;
+        case 0x12: printf("STAX   D"); break;
+        case 0x13: printf("INX    D"); break;
+        case 0x14: printf("INR    D"); break;
+        case 0x15: printf("DCR    D"); break;
+        case 0x16: printf("MVI    D,#$%02x", code[1]); opbytes = 2; break;
+        case 0x17: printf("RAL"); break;
+        case 0x18: printf("NOP"); break;
+        case 0x19: printf("DAD    D"); break;
+        case 0x1a: printf("LDAX   D"); break;
+        case 0x1b: printf("DCX    D"); break;
+        case 0x1c: printf("INR    E"); break;
+        case 0x1d: printf("DCR    E"); break;
+        case 0x1e: printf("MVI    E,#$%02x", code[1]); opbytes = 2; break;
+        case 0x1f: printf("RAR"); break;
+        case 0x20: printf("RIM"); break;
+        case 0x21: printf("LXI    H,#$%02x%02x", code[2], code[1]); opbytes = 3; break;
+        case 0x22: printf("SHLD   $%02x%02x", code[2], code[1]); opbytes = 3; break;
+        case 0x23: printf("INX    H"); break;
+        case 0x24: printf("INR    H"); break;
+        case 0x25: printf("DCR    H"); break;
+        case 0x26: printf("MVI    H,#$%02x", code[1]); opbytes = 2; break;
+        case 0x27: printf("DAA"); break;
+        case 0x28: printf("NOP"); break;
+        case 0x29: printf("DAD    H"); break;
+        case 0x2a: printf("LHLD   $%02x%02x", code[2], code[1]); opbytes = 3; break;
+        case 0x2b: 
+        case 0x2c:
+        case 0x2d:
+        case 0x2e:
+        case 0x2f:
+        case 0x30:
+        case 0x31:
+        case 0x32:
+        case 0x33:
+        case 0x34:
+        case 0x35:
+        case 0x36:
+        case 0x37:
+        case 0x38:
+        case 0x39:
+        case 0x3a:
+        case 0x3b:
+        case 0x3c:
+        case 0x3d:
+        case 0x3e:
+        case 0x3f:
+        case 0x40:
+        case 0x41:
+        case 0x42:
+        case 0x43:
+        case 0x44:
+        case 0x45:
+        case 0x46:
+        case 0x47:
+        case 0x48:
+        case 0x49:
+        case 0x4a:
+        case 0x4b:
+        case 0x4c:
+        case 0x4d:
+        case 0x4e:
+        case 0x4f:
+        case 0x50:
+        case 0x51:
+        case 0x52:
+        case 0x53:
+        case 0x54:
+        case 0x55:
+        case 0x56:
+        case 0x57:
+        case 0x58:
+        case 0x59:
+        case 0x5a:
+        case 0x5b:
+        case 0x5c:
+        case 0x5d:
+        case 0x5e:
+        case 0x5f:
+        case 0x60:
+        case 0x61:
+        case 0x62:
+        case 0x63:
+        case 0x64:
+        case 0x65:
+        case 0x66:
+        case 0x67:
+        case 0x68:
+        case 0x69:
+        case 0x6a:
+        case 0x6b:
+        case 0x6c:
+        case 0x6d:
+        case 0x6e:
+        case 0x6f:
+        case 0x70:
+        case 0x71:
+        case 0x72:
+        case 0x73:
+        case 0x74:
+        case 0x75:
+        case 0x76:
+        case 0x77:
+        case 0x78:
+        case 0x79:
+        case 0x7a:
+        case 0x7b:
+        case 0x7c:
+        case 0x7d:
+        case 0x7e:
+        case 0x7f:
+        case 0x80:
+        case 0x81:
+        case 0x82:
+        case 0x83:
+        case 0x84:
+        case 0x85:
+        case 0x86:
+        case 0x87:
+        case 0x88:
+        case 0x89:
+        case 0x8a:
+        case 0x8b:
+        case 0x8c:
+        case 0x8d:
+        case 0x8e:
+        case 0x8f:
+        case 0x90:
+        case 0x91:
+        case 0x92:
+        case 0x93:
+        case 0x94:
+        case 0x95:
+        case 0x96:
+        case 0x97:
+        case 0x98:
+        case 0x99:
+        case 0x9a:
+        case 0x9b:
+        case 0x9c:
+        case 0x9d:
+        case 0x9e:
+        case 0x9f:
+        case 0xa0:
+        case 0xa1:
+        case 0xa2:
+        case 0xa3:
+        case 0xa4:
+        case 0xa5:
+        case 0xa6:
+        case 0xa7:
+        case 0xa8:
+        case 0xa9:
+        case 0xaa:
+        case 0xab:
+        case 0xac:
+        case 0xad:
+        case 0xae:
+        case 0xaf:
+        case 0xb0:
+        case 0xb1:
+        case 0xb2:
+        case 0xb3:
+        case 0xb4:
+        case 0xb5:
+        case 0xb6:
+        case 0xb7:
+        case 0xb8:
+        case 0xb9:
+        case 0xba:
+        case 0xbb:
+        case 0xbc:
+        case 0xbd:
+        case 0xbe:
+        case 0xbf:
+        case 0xc0:
+        case 0xc1:
+        case 0xc2:
+        case 0xc3:
+        case 0xc4:
+        case 0xc5:
+        case 0xc6:
+        case 0xc7:
+        case 0xc8:
+        case 0xc9:
+        case 0xca:
+        case 0xcb:
+        case 0xcc:
+        case 0xcd:
+        case 0xce:
+        case 0xcf:
+        case 0xd0:
+        case 0xd1:
+        case 0xd2:
+        case 0xd3:
+        case 0xd4:
+        case 0xd5:
+        case 0xd6:
+        case 0xd7:
+        case 0xd8:
+        case 0xd9:
+        case 0xda:
+        case 0xdb:
+        case 0xdc:
+        case 0xdd:
+        case 0xde:
+        case 0xdf:
+        case 0xe0:
+        case 0xe1:
+        case 0xe2:
+        case 0xe3:
+        case 0xe4:
+        case 0xe5:
+        case 0xe6:
+        case 0xe7:
+        case 0xe8:
+        case 0xe9:
+        case 0xea:
+        case 0xeb:
+        case 0xec:
+        case 0xed:
+        case 0xee:
+        case 0xef:
+        case 0xf0:
+        case 0xf1:
+        case 0xf2:
+        case 0xf3:
+        case 0xf4:
+        case 0xf5:
+        case 0xf6:
+        case 0xf7:
+        case 0xf8:
+        case 0xf9:
+        case 0xfa:
+        case 0xfb:
+        case 0xfc:
+        case 0xfd:
+        case 0xfe:
+        case 0xff: break;
+    }    
+    printf("\n");
+
+    return opbytes;
+}
+
+int main(int argc, char** argv)
+{
+    if(argc < 2)
+    {
+        printf("Please select a file to disassemble.\n");
+        exit(1);
+    }
+
+    FILE *f = fopen(argv[1], "rb"); //"b" is for compatibility with other systems
+    if (f==NULL)
+    {
+        printf("Error: Couldnt open %s\n", argv[1]);
+        exit(1);
+    }
+
+    //Get the file size and read it into a memory buffer
+    fseek(f, 0L, SEEK_END);
+    int fsize = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+
+    unsigned char *buffer = malloc(fsize);
+
+    fread(buffer, fsize, 1, f);
+    fclose(f);
+
+    int pc = 0;
+
+    while (pc < fsize)
+    {
+        pc + Disassemble8080Op(buffer, pc);
+    }
+    
+    return 0;
+}
